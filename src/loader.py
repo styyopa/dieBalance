b5f796ad3f99b6ff574d4e91045230ed5028b541
Vladyslav.S <vladstepanok2@gmail.com>
2026-06-09 11:25:23 +0200

    add fat and fiber parsing with safe float conversion

--- src/loader.py
+++ src/loader.py
@@ -1,7 +1,11 @@
 import csv
 
-
-
+def save_float(value):
+    try:
+        return float(value) if value else 0.0
+    except ValueError:
+        return 0.0 
+    
 def load_foods():
 
     foods = []
@@ -9,22 +13,18 @@ def load_foods():
     with open("data/foods.csv", newline="", encoding="utf-8") as file:
         
         reader = csv.DictReader(file)
-
+        
         for row in reader:
-            def safe_float(value):
-                try:
-                    return float(value) if value else 0.0
-                except ValueError:
-                    return 0.0
+            
             food = {
-                "name": row["name"],
-                "carbs": float(row["Carbohydrates"]),
-                "glycemic_index": float(row["Glycemic Index"]),
-                "protein": float(row["Protein"]),
-                "fiber": float(row["Fiber"]),
-                "fat": float(row["Fat"], 0)
+                "name": row("name"),
+                "carbs": save_float(row("Carbohydrates")),
+                "glycemic_index": save_float(row("Glycemic Index")),
+                "protein": save_float(row("Protein")),
+                "fiber": save_float(row("Fiber")),
+                "fat": save_float(row("Fat")) 
             }
-
-            foods.append(food)
+    
+        foods.append(food)
 
     return foods
